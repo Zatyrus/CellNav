d707efd43d22e4c98f64f8c0972c595abb4776c7
Bela Vogler <bela.vogler@uni-jena.de>
2026-06-12 21:11:30 +0200

    Create path_3d.py

---
+++ Min3D/core/containers/path_3d.py
@@ -0,0 +1,17 @@
+import open3d as o3d
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Path3D:
+    # base geometry attributes
+    edges: o3d.geometry.LineSet
+
+    # additional attributes for visualization
+    start_point: o3d.geometry.TriangleMesh
+    end_point: o3d.geometry.TriangleMesh
+    intermediate_points: List[o3d.geometry.TriangleMesh]
+
+    def to_list(self) -> List[o3d.geometry.Geometry]:
+        return [self.edges, self.start_point, self.end_point] + self.intermediate_points
